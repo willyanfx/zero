1c17d0b5a8e0cd2c8dcf1bed2b0a87216be956af
willyanfx <willyan.fx@gmail.com>
2022-10-26 22:24:08 -0700

    main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("hello {}!", &name)
-}
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use std::net::TcpListener;
+use zero::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().route("/health_check", web::get().to(health_check))
-        // .route("/", web::get().to(greet))
-        // .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
