79bdfcf0d47a80abbad7b100d02f441478c33fe5
willyanfx <willyan.fx@gmail.com>
2022-11-02 21:26:52 -0700

    added support to config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::net::TcpListener;
+use zero::configuration::get_configuration;
 use zero::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
