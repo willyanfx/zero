389f0cf01fa23a1b923cfb3cac08d5b2ba2070f4
willyanfx <willyan.fx@gmail.com>
2022-11-08 17:43:33 -0800

    Added support to Pool connection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero::configuration::get_configuration;
-use zero::run;
-
+use zero::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
